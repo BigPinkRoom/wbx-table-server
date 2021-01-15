b7a1a43898bce1ceeb1c3522f44bd8a82abcdc42
Tester <TestTestTest1000@Test.com.ru>
2021-01-15 14:47:44 +0300

    create base logic in main index.js file

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const apiTableRoute = require('./routes/api-table');
+const errorHandler = require('./middleware/error');
+const keys = require('./keys');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(helmet());
+
+app.use('/api/table', apiTableRoute);
+app.use(errorHandler);
+
+async function start() {
+  try {
+    await mongoose.connect(keys.MONGODB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+start();
